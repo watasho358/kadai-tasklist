53edd173ef85c64ead979a82bbbaf96b517b9d0d
shota.watanabe <t8mmmxc10y@yahoo.co.jp>
2018-10-08 09:53:07 +0000

    add validation column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -28,7 +28,14 @@ class TasksController extends Controller
 
     public function store(Request $request)
     {
+        
+     $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status;  //追加
         $task->content = $request->content;
         $task->save();
 
@@ -55,7 +62,13 @@ class TasksController extends Controller
 
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status;  //追加
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,6 +4,7 @@
 
     <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
 
+    <p>ステータス: {{ $task->status }}</p>
     <p>{{ $task->content }}</p>
 
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
