f16c3b07c727ac9828d0c981577af96b3f38f059
shota.watanabe <t8mmmxc10y@yahoo.co.jp>
2018-10-14 13:43:25 +0000

    bootstrap design4

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-<h1>タスク新規作成ページ</h1>
-
 <div class="row">
-    <div class="col-xs-12" "col-sm-offset-2 col-sm-6" "col-md-offset-2 col-md-6" "col-lg-offset-3 col-lg-3">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             
+    <h1>タスク新規作成ページ</h1>
+    
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
         <div class="form-group">
@@ -22,7 +22,7 @@
         {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
-</div>
     </div>
+</div>
 
 @endsection
