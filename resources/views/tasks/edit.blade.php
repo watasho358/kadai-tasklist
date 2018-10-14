cbbc6ef61c7a5d565ee512883fadcb8608745222
shota.watanabe <t8mmmxc10y@yahoo.co.jp>
2018-10-14 12:12:06 +0000

    kadai bootstrap design3

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+<h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
 <div class="row">
     <div class="col-xs-12" "col-sm-offset-2 col-sm-6" "col-md-offset-2 col-md-6" "col-lg-offset-3 col-lg-3">
@@ -10,13 +10,13 @@
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
         <div class="form-group">
-　　　　{!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
         </div>
         
         <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
         </div>
         
         {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
