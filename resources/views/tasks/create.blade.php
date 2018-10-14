7264da2d41f0fb6fdb0f7ddb85b58444aa0c5848
shota.watanabe <t8mmmxc10y@yahoo.co.jp>
2018-10-14 07:14:44 +0000

    kadai bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,18 +4,31 @@
 
 <h1>タスク新規作成ページ</h1>
 
+<div class="row">
+    <div class="col-xs-12">
+    <div class="col-sm-8">
+    <div class="col-md-8">
+    <div class="col-lg-6">
+            
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
+        <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
 
-
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
-
+</div>
+    </div>
+    </div>
+    </div>
+    </div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,16 +4,30 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
+<div class="row">
+    <div class="col-xs-12">
+    <div class="col-sm-8">
+    <div class="col-md-8">
+    <div class="col-lg-6">
+        
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
+        <div class="form-group">
 　　　　{!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status') !!}
+        </div>
         
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+        </div>
+        
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
-
+</div>
+    </div>
+    </div>
+    </div>
+    </div>
 @endsection
