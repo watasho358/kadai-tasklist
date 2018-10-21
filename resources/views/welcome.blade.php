81ca25112e30a903a95982661a30e106e9dc9dc4
shota.watanabe <t8mmmxc10y@yahoo.co.jp>
2018-10-21 02:46:10 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
+@if (Auth::check())
+        <?php $user = Auth::user(); ?>
+        <header>
+    <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">TaskList</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                    <li><a href="#">Login</a></li>
+                    /* <li>{!! link_to_route('tasks.create', '新規タスクの作成') !!}</li> */
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the TaskList</h1>
